328efa5b7cefa41111a32867947ecb6ff06fc72b
benblack <weepingwillowben@gmail.com>
2016-07-29 12:32:54 -0700

    fixed date default

--- strabo/private_helper.py
+++ strabo/private_helper.py
@@ -14,13 +14,9 @@ from strabo import app
 from strabo import db
 from strabo import straboconfig
 
-def make_interest_point(ip_id,images,form_title,form_body,form_geo_obj,form_layer,form_icon):
-    if ip_id:
-        ip = db.session.query(schema.InterestPoints).get(ip_id)
-    else:
-        ip = schema.InterestPoints()
-        db.session.add(ip)
-        db.session.flush()
+def make_interest_point(form_ip_id,images,form_title,form_body,form_geo_obj,form_layer,form_icon):
+
+    ip = db.session.query(schema.InterestPoints).get(form_ip_id) if form_ip_id else schema.InterestPoints()
 
     ip.title = form_title
     ip.descrip_body = form_body
@@ -30,18 +26,31 @@ def make_interest_point(ip_id,images,form_title,form_body,form_geo_obj,form_laye
     ip.images = images
     return ip
 
-def make_image(ip_idx,form_image_id,form_file_obj,form_descrip,year,month):
+def make_date(form_year,form_month):
+    '''
+    :param string form_year: Year value passed in from form. Is a string decimal
+        representation of a year or en empty string if that part of the form was
+        left empty.
+
+    :param string form_month: Month value, follows same format as year.
+
+    If month and year are valid values,returns date with specied month and year.
+
+    If not, then it uses today's date as a default.
+    '''
+    default_day = 1
+    year = int(form_year) if form_year else datetime.date.today().year
+    month = int(form_month) if form_month else datetime.date.today().month
+
+    return  datetime.date(year,month,default_day)
+
+def make_image(ip_idx,form_image_id,form_file_obj,form_descrip,form_year,form_month):
     '''
     if a flask.files object is passed in, then
     '''
-    if form_image_id != '':
-        image = db.session.query(schema.Images).get(form_image_id)
-    else:
-        image = schema.Images()
-        db.session.add(image)
+    image = db.session.query(schema.Images).get(form_image_id) if form_image_id else schema.Images()
 
-    default_day = 1
-    image.taken_at = datetime.date(utils.safe_pos_int_conv(year),utils.safe_pos_int_conv(month),1)
+    image.taken_at = make_date(form_year,form_month)
     image.description = form_descrip
     image.ip_order_idx = ip_idx
 
